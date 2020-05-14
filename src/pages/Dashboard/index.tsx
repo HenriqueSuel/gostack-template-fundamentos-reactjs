bfad8b20a617a0e3335af7f9222a5c1e160a14f2
Henrique <h.suel17@hotmail.com>
2020-05-14 11:56:10 -0300

    finalizado os teste

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { useState, useEffect } from 'react';
 
 import income from '../../assets/income.svg';
@@ -24,20 +25,39 @@ interface Transaction {
 }
 
 interface Balance {
-  income: string;
-  outcome: string;
-  total: string;
+  income: number;
+  outcome: number;
+  total: number;
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+/*   const formatMoney = (value: number, category: string): string => {
+    if (category === 'outcome') {
+      return ` - R$ ${Intl.NumberFormat().format(value)},00`;
+    }
+    return ` R$ ${Intl.NumberFormat().format(value)},00`;
+  }; */
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
-    }
+      api.get('/transactions').then(resp => {
+        const {
+          transactions: respTransactions,
+          balance: respBalance,
+        } = resp.data;
 
+        const teste = respTransactions.map((r: Transaction) => {
+          r.formattedValue = formatValue(r.value);
+          return r;
+        });
+
+        setTransactions(teste);
+        setBalance(respBalance);
+      });
+    }
     loadTransactions();
   }, []);
 
@@ -51,21 +71,25 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">
+              R$ {formatValue(balance.income)}
+            </h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">
+              R$ {formatValue(balance.outcome)}
+            </h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">R$ {formatValue(balance.total)}</h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +105,21 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && ' - '}
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>
+                    {new Intl.DateTimeFormat('pt-BR').format(
+                      new Date(transaction.created_at),
+                    )}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ReactHTMLElement } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import filesize from 'filesize';
@@ -23,19 +23,25 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file, uploadedFiles[0].name);
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesTratada = files.map(file => {
+      return {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+    });
+    setUploadedFiles([...filesTratada]);
   }
 
   return (
